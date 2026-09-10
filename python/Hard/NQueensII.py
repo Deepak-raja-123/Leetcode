6f677272ef4d0b37e9467000c6532bc0561cad1f
Deepak-raja-123 <deepak14130l111@gmail.com>
2026-09-10 20:37:05 +0530

    Solve LeetCode #52 - N-Queens II

---
+++ python/Hard/NQueensII.py
@@ -0,0 +1,35 @@
+class Solution:
+    def totalNQueens(self, n):
+        cols = set()
+        pos_diag = set()
+        neg_diag = set()
+
+        def backtrack(row):
+            if row == n:
+                return 1
+
+            count = 0
+
+            for col in range(n):
+                if col in cols:
+                    continue
+
+                if row + col in pos_diag:
+                    continue
+
+                if row - col in neg_diag:
+                    continue
+
+                cols.add(col)
+                pos_diag.add(row + col)
+                neg_diag.add(row - col)
+
+                count += backtrack(row + 1)
+
+                cols.remove(col)
+                pos_diag.remove(row + col)
+                neg_diag.remove(row - col)
+
+            return count
+
+        return backtrack(0)
